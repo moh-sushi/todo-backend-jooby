81ec74e29b276c07971dc4f8a8609e85229150f6
Sascha Fischer <moh.sushi@googlemail.com>
2021-11-17 07:09:52 +0100

    added / for path

--- src/main/java/com/todobackend/mohsushi/App.java
+++ src/main/java/com/todobackend/mohsushi/App.java
@@ -8,7 +8,7 @@ public class App extends Jooby {
     install(new JacksonModule());
 
     install("/todos", () -> new TodosApp());
-    install("version",() -> new VersionApp());
+    install("/version",() -> new VersionApp());
   }
 
   public static void main(final String[] args) {
